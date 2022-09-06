94a76b41c8137214fbf29e92d42a422d00b9f5c7
filipeas <felipealvessampaio@hotmail.com>
2022-09-06 20:34:12 -0300

    add eps 1

--- eps1/eps1.hs
+++ eps1/eps1.hs
@@ -1,13 +1,32 @@
 {--
 1)
 --}
+class Visible a where
+    toString :: a -> String
+    size :: a -> Int
 
+instance Visible Bool where
+    toString True = "True"
+    toString _ = "False"
+    size _ = 1
 
-{--
-2)
---}
+instance Visible (a, b) where
+    toString (a, b) = "True"
+    size (_, _) = 2
 
-{--
-3)
---}
-compare x y = size x <= size y
+instance Visible (a, b, c) where
+    toString (a, b, c) = "True"
+    size (_, _, _) = 3
+
+-- {--
+-- 2)
+-- --}
+converte :: Int -> String
+converte n = show n
+instance Visible Int where
+    toString n = converte n
+    size _ = 0
+
+-- {--
+-- 3) compare x y = size x <= size y é numérico
+-- --}
